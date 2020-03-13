412453904310bb83f7a334d1a7b7968d4108f90b
Азимбай Кубанычбеков <azimku7@mail.ru>
2020-03-13 23:52:45 +0600

    happy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import student from './student'
+import Student from'./Student';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>hello world!</h1>
+     <Student/>
     </div>
   );
 }
---
+++ src/Student.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Student(props) {
+    return (
+        <div>
+           <h1> Hello i'm Student</h1>
+   
+            </div>
+    );
+};
+
+
+
+export default Student;
