1c0d5dd382a8d7e89a930969dbafdecc85cce8fd
Азимбай Кубанычбеков <azimku7@mail.ru>
2020-04-17 22:10:51 +0600

    import repair

--- src/components/layout/logo/Logo.js
+++ src/components/layout/logo/Logo.js
@@ -3,7 +3,7 @@ import classes from './logo.module.css';
 
 export default () => (
   <div className={classes.Logo}>
-    <img src="https://freesvg.org/img/Gerald_G_Fast_Food_Lunch_Dinner_%28FF_Menu%29_6.png"/> <br/>
+    
     <span>Burger Builder</span>
   </div>
 );
