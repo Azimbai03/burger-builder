49512499691dd1e27ead8f91bae87523bf2d3f7e
Азимбай Кубанычбеков <azimku7@mail.ru>
2020-04-19 22:31:24 +0600

    aded import

--- src/components/layout/Toolbar/Toolbar.js
+++ src/components/layout/Toolbar/Toolbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Nav from './Nav/Nav';
-import Logo from '../Logo/Logo';
+import Logo from "../Logo/Logo";
 
 export default () =>  (
   <div className={classes.Toolbar}>
