7957a9aebb2523f7c0e9bd2e9c06fc3fe369e435
Азимбай Кубанычбеков <azimku7@mail.ru>
2020-05-05 05:09:58 +0600

    changes to bulka

--- src/components/Bulka/BulkaBottom.js
+++ src/components/Bulka/BulkaBottom.js
@@ -4,7 +4,7 @@ import classes from './BulkaBottom.module.css';
 export default ({ children }) => {
 
   return (
-    <div className={classes.BulkaBottom}>
+    <div className={classes.bulkabottom}>
     </div>
   );
 };
