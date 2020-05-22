062db96e3ba64e4a8e48f3b91c9315f4ca1dc9b1
Азимбай Кубанычбеков <azimku7@mail.ru>
2020-05-22 10:40:57 +0600

    changes

--- src/conteiners/Burgerbuilder.js
+++ src/conteiners/Burgerbuilder.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import axios from "../axios";
 import BurgerKit from "../components/BurgerBuilder/BurgerKit/BurgerKit";
 import BurgerControls from "../components/BurgerBuilder/BurgerControls/BurgerControls";
 import Modal from "../components/UI/Modal/Modal";
 import OrderSummary from "../components/BurgerBuilder/OrderSummary/OrderSummary";
 import Loading from "../components/UI/Loading/Loading";
-import classes from "./BurgerBuilder.module.css";
-import { useHistory } from "react-router-dom";
 import withErrorHandler from "../hoc/withErrorHandler";
+import classes from "./BurgerBuilder.module.css";
+
+
 const PRICES = {
   chees: 15.38,
   steak: 5.5,
@@ -15,90 +16,61 @@ const PRICES = {
   lettuce: 10.4,
   
 };
-export default withErrorHandler(() => {
-  const [ingredients, setIngredients] = useState(null);
-  const [price, setPrice] = useState(100);
+export default () => {
+  const [ingredients, setIngredients] = useState({
+    chees: 0,
+    steak: 0,
+   tamato: 0,
+    lettuce: 0,
+    
+   });
+  const [price, setPrice] = useState(40);
   const [canOrder, setCanOrder] = useState(false);
   const [isOrdering, setIsOrdering] = useState(false);
-  const history = useHistory();
-
+  const [loading, setLoading] = useState(false);
   function checkCanOrder(ingredients) {
     const total = Object.keys(ingredients).reduce((total, ingredient) => {
       return total + ingredients[ingredient];
     }, 0);
     setCanOrder(total > 0);
   }
-
   function startOrder() {
     setIsOrdering(true);
   }
-
   function cancelOrder() {
     setIsOrdering(false);
   }
-
   function finishOrder() {
-    const queryParams = Object.keys(ingredients).map(
-      (ingredient) =>
-        encodeURIComponent(ingredient) +
-        "=" +
-        encodeURIComponent(ingredients[ingredient])
-    );
-    queryParams.push("price=" + encodeURIComponent(price.toFixed(2)));
+    const order = {
+      ingredients: ingredients,
+      price: price,
+      delivery: "Fast",
+      customer: {
+        name: "Bakyt",
+        phone: "0700700700",
+        address: {
+          street: "123 Gebze",
+          city: "Karakol",
+        },
+      },
+    };
 
-    history.push({
-      pathname: "/checkout",
-      search: queryParams.join("&"),
+setLoading(true);
+     axios.post("/orders.json", order).then((response) => 
+    {setLoading(false);
+      setIsOrdering(false);
     });
   }
-
   function addIngredient(type) {
     const newIngredients = { ...ingredients };
     newIngredients[type]++;
     setIngredients(newIngredients);
     checkCanOrder(newIngredients);
-
     const newPrice = price + PRICES[type];
     setPrice(newPrice);
   }
-
-  function removeIngredient(type) {
-    if (ingredients[type] >= 1) {
-      const newIngredients = { ...ingredients };
-      newIngredients[type]--;
-      setIngredients(newIngredients);
-      checkCanOrder(newIngredients);
-
-      const newPrice = price - PRICES[type];
-      setPrice(newPrice);
-    }
-  }
-
-  useEffect(() => {
-    axios
-      .get("/Orders.json")
-      .then((response) => setIngredients(response.data))
-      .catch((error) => {});
-  }, []);
-
-  let output = <Loading />;
-  if (ingredients) {
-    output = (
-      <>
-        <BurgerKit price={price} ingredients={ingredients} />
-        <BurgerControls
-          startOrder={startOrder}
-          canOrder={canOrder}
-          ingredients={ingredients}
-          addIngredient={addIngredient}
-          removeIngredient={removeIngredient}
-        />
-      </>
-    );
-  }
-
-  let orderSummary = <Loading />;
-  if (isOrdering) {
+  let orderSummary = <Loading/>;
+  if (!loading) {
     orderSummary = (
       <OrderSummary
         ingredients={ingredients}
@@ -108,14 +80,29 @@ export default withErrorHandler(() => {
       />
     );
   }
-
+  function removeIngredient(type) {
+    if (ingredients[type] >= 1) {
+      const newIngredients = { ...ingredients };
+      newIngredients[type]--;
+      setIngredients(newIngredients);
+      checkCanOrder(newIngredients);
+      const newPrice = price - PRICES[type];
+      setPrice(newPrice);
+    }
+  }
   return (
     <div className={classes.BurgerBuilder}>
-      <h1>Buger builder</h1>
-      {output}
+      <BurgerKit price={price} ingredients={ingredients} />
+      <BurgerControls
+        startOrder={startOrder}
+        canOrder={canOrder}
+        ingredients={ingredients}
+        addIngredient={addIngredient}
+        removeIngredient={removeIngredient}
+      />
       <Modal show={isOrdering} hideCallback={cancelOrder}>
-        {orderSummary}
+       {orderSummary}
       </Modal>
     </div>
   );
-}, axios);
+};
